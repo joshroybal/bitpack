4728d3d5ad74c5d051a41109bc2665bbd1fa8e82
joshroybal <slacker@darkstar.local>
2019-11-14 13:37:07 -0600

    removed debugging display functions that are no longer needed

--- src/bitpack.h
+++ src/bitpack.h
@@ -6,8 +6,5 @@ long pack(char *);
 void unpack(char [], long);
 void packarr(char *, char *);
 void unpackarr(char *, char *);
-void display_ascii(char);
-void display_byte(char);
-void display_word(long);
 
 #endif
